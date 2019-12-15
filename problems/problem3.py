f5d39d064951a92f295121c5d8d635c8aaefb11c
Håvard Moe Jacobsen <haavmj18@student.uia.no>
2019-12-15 22:38:54 +0100

    .

--- problems/problem3.py
+++ problems/problem3.py
@@ -2,55 +2,22 @@
 The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143 ?
 """
-from itertools import product
-import time
 import math
-
 
 def finding_primes():
     pass
 
 
-def is_prime_v1(num):
-    if num == 1:
-        return False  # 1 is not a prime
-    for i in range(2, num):
-        if num % i == 0:
-            return False
-    return True
+def is_prime(num):
+    if num < 2: return False  # no number under 2 is a prime
 
+    elif num == 2: return True  # 2 is the only even prime number
 
-def is_prime_v2(num):
-    if num == 1:
-        return False  # 1 is not a prime
-    max_divisor = math.floor(math.sqrt(num))
-    for i in range(2, max_divisor+1):
-        if num % i:
-            return False
-    return True
-
-def is_prime_v3(num):
-    if num < 2:
-        return False  # no number under 2 is a prime
-
+    elif num % 2 == 0: return False
 
     max_divisor = math.floor(math.sqrt(num))
-    for i in range(2, max_divisor+1):
-        if num % i:
-            return False
-    return True
+    return all(num % divisor != 0 for divisor in range(3, max_divisor+1, 2))
 
 
 if __name__ == '__main__':
-    t0 = time.time()
-    for n in range(1, 50000):
-        is_prime_v1(n)
-    t1 = time.time()
-    time_v1 = t1 - t0
-
-    t0 = time.time()
-    for n in range(1, 50000):
-        is_prime_v2(n)
-    t1 = time.time()
-    time_v2 = t1 - t0
-    print(f"time taken v1: {time_v1}\ntime taken v2: {time_v2}")
+    print(is_prime(97))
--- problems/test.py
+++ problems/test.py
@@ -1,4 +1,22 @@
 import math
+import numpy as np
+
+def is_prime(num):
+    if num < 2: return False
+    max_divisor = int(math.sqrt(num))
+
+    return all(num % i != 0 for i in range(2, max_divisor+1))
+
+def primes(U):
+    sieve = np.full(U, True, dtype=bool)
+    sieve[0] = sieve[1] = False
+
+    for i in range(U):
+        if not sieve[i]: continue
+        sieve[i*i::i] = False
+        yield i
 
 if __name__ == '__main__':
-    print(math.floor(4.6666))
+    print(math.sqrt(600851475143))
+
+
